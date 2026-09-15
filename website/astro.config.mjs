9c845355e5ef6c023d509518d84403a45b70707b
Ján Polaček <janpolacek@outlook.com>
2026-09-15 19:16:59 +0200

    Deploy website to GitHub Pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const deploymentBase = process.env.DEPLOYMENT_BASE;
+const siteUrl = process.env.SITE_URL;
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  ...(deploymentBase ? { base: deploymentBase } : {}),
+  ...(siteUrl ? { site: siteUrl } : {}),
 });
